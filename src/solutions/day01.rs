6459c3da7df9d778853f4bec8e9a797fca2d89bd
Piyush Jaipuriyar <piyushjaipuriyar@gmail.com>
2025-12-03 00:01:07 +0530

    solve: day1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -9,13 +9,50 @@ pub fn solve(input: &str, part: u8) -> Result<String> {
 }
 
 fn solve_part1(input: &str) -> Result<String> {
-    // TODO: Implement part 1
-    Ok("Not yet implemented".to_string())
+    let mut position: i32 = 50;
+    let mut result: i32 = 0;
+    for line in input.lines() {
+        let (instruction, step) = parse_instruction(line);
+        position = rotate(&position, &instruction, &step);
+        result = result + (if position == 0 { 1 } else { 0 });
+    }
+    Ok(result.to_string())
 }
 
 fn solve_part2(input: &str) -> Result<String> {
-    // TODO: Implement part 2
-    Ok("Not yet implemented".to_string())
+    let mut position: i32 = 50;
+    let mut result: i32 = 0;
+    for line in input.lines() {
+        let (instruction, step) = parse_instruction(line);
+        let mut stepper: i32 = step;
+        let amount = match instruction.as_str() {
+            "L" => -1,
+            "R" => 1,
+            _ => 0,
+        };
+        while stepper > 0 {
+            position = (position - amount).rem_euclid(100);
+            if position == 0 {
+                result += 1;
+            }
+            stepper -= 1;
+        }
+    }
+    Ok(result.to_string())
+}
+
+fn rotate(current: &i32, direction: &str, step: &i32) -> i32 {
+    match direction {
+        "L" => ((current - step) % 100 + 100) % 100,
+        "R" => (current + step) % 100,
+        _ => *current,
+    }
+}
+
+fn parse_instruction(instruction: &str) -> (String, i32) {
+    let direction = instruction.chars().next().unwrap().to_string();
+    let step = instruction[1..].parse::<i32>().unwrap();
+    (direction, step)
 }
 
 #[cfg(test)]
